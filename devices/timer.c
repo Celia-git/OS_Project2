f2257bbe3b6c19b8984cc7e3ac7b5e79493f4f36
ddsola93 <ddsola43@tntech.edu>
2025-11-28 22:50:45 -0600

    wake sleeping threads in the timer interrupt

--- devices/timer.c
+++ devices/timer.c
@@ -197,6 +197,18 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
+  /* Wake up threads whose wake_tick <= current tick. */
+while (!list_empty (&sleep_list))
+  {
+    struct thread *t =
+      list_entry (list_front (&sleep_list), struct thread, elem);
+
+    if (t->wake_tick > ticks)
+      break;
+
+    list_pop_front (&sleep_list);
+    thread_unblock (t);
+  }
   thread_tick ();
 }
 
