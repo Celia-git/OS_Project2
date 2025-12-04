2589a41392644f2efaf58aa7abf49adb2b63d713
Celia Hough <celia.hough.programmer@gmail.com>
2025-12-03 18:10:55 -0600

    Update timer.h

--- devices/timer.h
+++ devices/timer.h
@@ -6,7 +6,7 @@
 #include <list.h>
 #include <stdbool.h>
 
-/* Number of timer interrupts per second. */
+/* [translate:Number of timer interrupts per second.] */
 #define TIMER_FREQ 100
 
 void timer_init (void);
@@ -15,13 +15,13 @@ void timer_calibrate (void);
 int64_t timer_ticks (void);
 int64_t timer_elapsed (int64_t);
 
-/* Sleep and yield the CPU to other threads. */
+/* [translate:Sleep and yield the CPU to other threads.] */
 void timer_sleep (int64_t ticks);
 void timer_msleep (int64_t milliseconds);
 void timer_usleep (int64_t microseconds);
 void timer_nsleep (int64_t nanoseconds);
 
-/* Busy waits. */
+/* [translate:Busy waits.] */
 void timer_mdelay (int64_t milliseconds);
 void timer_udelay (int64_t microseconds);
 void timer_ndelay (int64_t nanoseconds);
